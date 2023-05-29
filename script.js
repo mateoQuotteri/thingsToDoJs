31432f47e9e54410d2c21abb6c42699c1e920af4
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-29 20:46:42 -0300

    the script js tryng to work with errors

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const input = document.getElementById("newTaskInput")
 const ul = document.getElementById("taskList")
 
 
+form.addEventListener("submit", function () {
+    let tareaAAgregar = input.value;
+    console.log(tareaAAgregar);
+    // Crea un nuevo elemento <li>
+const newLiElement = document.createElement("li");
+
+// Crea un elemento <input> para la casilla de verificación
+const checkboxElement = document.createElement("input");
+checkboxElement.type = "checkbox";
+
+// Crea un elemento <span> para el texto de la tarea
+const spanElement = document.createElement("span");
+spanElement.textContent = tareaAAgregar;
+
+// Crea un botón de eliminación
+const deleteButton = document.createElement("button");
+deleteButton.classList.add("delete-button");
+deleteButton.textContent = "X";
+
+
+ul.appendChild(newLiElement);
+})
